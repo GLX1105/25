6db06b963b49f7358a50ceec9d9ed7b1529e7295
GLX1105 <562761508@qq.com>
2026-05-13 04:42:44 +0800

    Create hello.js

---
+++ api/hello.js
@@ -0,0 +1,11 @@
+export async function onRequest(context) {
+  return new Response(JSON.stringify({
+    message: '后端启动成功！',
+    time: new Date().toISOString()
+  }), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
+}
